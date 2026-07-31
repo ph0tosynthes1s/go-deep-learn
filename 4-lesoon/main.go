f6617b7599fc28c45dab4dae6a0e7049eee87387
k.pimenov <kostalexo31@gmail.com>
2026-07-31 23:41:42 +0300

    feat(4-lesson): fourth lesson - practice json

---
+++ 4-lesoon/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Record struct {
+	Nums string
+}
+
+func main() {
+	var myRecord Record
+
+	arguments := os.Args
+	if len(arguments) < 2 {
+		fmt.Println("not enough")
+	}
+
+	filename := arguments[1]
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("open failed")
+	}
+	defer file.Close()
+
+	decoderJSON := json.NewDecoder(file)
+
+	err = decoderJSON.Decode(&myRecord)
+	if err != nil {
+		fmt.Println("parse failed")
+	}
+	fmt.Println((myRecord))
+}
