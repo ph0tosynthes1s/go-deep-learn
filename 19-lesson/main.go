44835d9356a5a515dfbd4547f96ddaa01e9c5ec2
k.pimenov <kostalexo31@gmail.com>
2026-08-20 23:50:17 +0300

    feat(19-lesson): nineteenth lesson - practice goroutines with select and context

---
+++ 19-lesson/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
+var blocker = make(chan struct{})
+
+func routine(ctx context.Context, duration time.Duration) {
+	defer close(blocker)
+	select {
+	case <-time.After(duration):
+		fmt.Print("Yep!")
+		blocker <- struct{}{}
+	case <-ctx.Done():
+		fmt.Printf("Operation timeout: %v\n", ctx.Err())
+	}
+}
+
+func main() {
+	randomCount := rand.IntN(5) + 1 // тип int
+	randomDuration := time.Duration(randomCount) * time.Second
+	var ctx, cancel = context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+d
+	go routine(ctx, randomDuration)
+	<-blocker
+}
